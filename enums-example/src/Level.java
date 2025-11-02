6f7e776f1d2842a0beb09e8ba69af098f3cf7a79
unknown <rjdeshmukh111@gmail.com>
2025-11-02 20:18:22 +0530

    intial commit

---
+++ enums-example/src/Level.java
@@ -0,0 +1,19 @@
+public enum Level {
+    LOW("low"),
+    MEDIUM("medium"),
+    HIGH("high");
+
+    private String level;
+
+    Level(String level) {
+        this.level = level;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
---
+++ enums-example/src/LevelEnumDemo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LevelEnumDemo {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Please enter tempertaure level");
+        String levelString = scanner.nextLine();
+
+        Level temperatureLevel = Level.valueOf(levelString);
+        System.out.println("current temp level : " + temperatureLevel);
+
+        User user = new User();
+        user.id= 1;
+        user.name="r";
+        user.userType=UserType.CUSTOMER;
+        System.out.println("user: " + user);
+    }
+}
